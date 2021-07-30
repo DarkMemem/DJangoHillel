8a75ea4263fd09d4b08fba5a43b199d67e4f8cba
Eugen <tbigbangtheorythe@gmail.com>
2021-07-30 19:48:12 +0300

    Inherited Student and Teachers from Person

---
+++ teachers/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+from core.models import Person
+
+
+class Teachers(Person):
+    create_datetime = models.DateTimeField(auto_now_add=True)
+    update_datetime = models.DateTimeField(auto_now=True)
+
+    @classmethod
+    def _generate(cls):
+        super()._generate()
