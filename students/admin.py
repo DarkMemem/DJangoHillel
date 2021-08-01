0422d64d27fcc5b20650e0bb977c892bc4371726
Eugen <tbigbangtheorythe@gmail.com>
2021-08-02 01:59:58 +0300

    Configured groups in the Administrator panel - solve the issue of adding students to the group through the administrator panel

--- students/admin.py
+++ students/admin.py
@@ -1,6 +1,33 @@
-from django.contrib import admin # noqa
-
-# Register your models here.
+from django.contrib import admin
 from students.models import Students
 
-admin.site.register(Students)
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = [
+        'first_name',
+        'last_name',
+        'birthdate',
+        'groups_number',
+    ]
+
+    list_display_links = [
+        'first_name',
+        'last_name',
+        'birthdate',
+        'groups_number',
+    ]
+
+    list_per_page = 20
+
+    fields = [
+        ('first_name', 'last_name'),
+        ('birthdate', 'age'),
+        'email',
+        ('groups_number', 'enroll_date', 'graduate_date')
+    ]
+
+    readonly_fields = ['age']
+    search_fields = ['first_name', 'last_name']
+
+
+admin.site.register(Students, StudentAdmin)
